e2941d1f3b7cac0ff649d11ca05a73276f44d616
Peter Hurst <peter.hurst@sky.uk>
2023-12-21 22:25:29 +0000

    clean

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,9 +4,6 @@ exports.loginPage = (req, res) => {
   res.send('login page');
 };
 
-// passport uses strategies, the local strategy is commonly used for username and password..
-// ..authentication. Passport must be configured before using any strategy (handlers/passport.js)..
-// ..this local strategy will add the User obj on each request.
 exports.login = (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
@@ -32,25 +29,16 @@ exports.login = (req, res, next) => {
   })(req, res, next);
 };
 
-
-exports.isLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next(); // They are logged in!
-    return;
-  }
-};
-
 exports.logout = (req, res) => {
   req.logout();
   res.json({ success: true, message: 'Logout successful' });
   res.redirect('/');
 };
 
-exports.isLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated) {
-    next();
-    return;
+exports.isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
   }
 
-  res.json('User not authorised.');
+  res.status(401).json({ message: 'User unauthorized' });
 };
