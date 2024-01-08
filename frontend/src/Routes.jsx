2b53b25e969a0f7932fd49aa15e09372222c1c88
Peter Hurst <peter.hurst@sky.uk>
2024-01-08 14:52:59 +0000

    missed from prior commit

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes as RRDRoutes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import HomeLoggedIn from "./pages/HomeLoggedIn";
+import { useAuth } from "./components/context/AuthContext";
+
+const PrivateOutlet = ({ userIsAuthenticated }) => {
+  console.log("PrivateOutlet", userIsAuthenticated);
+
+  if (userIsAuthenticated === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  return userIsAuthenticated === "Authorised" ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/unauthorised" />
+  );
+};
+
+const Routes = () => {
+  const [userIsAuthenticated, setUserIsAuthenticated] = useState();
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    setUserIsAuthenticated(isAuthenticated);
+  }, [isAuthenticated]);
+
+  return (
+    <Router>
+      <RRDRoutes>
+        <Route
+          path="/"
+          element={<Home />}
+          userIsAuthenticated={userIsAuthenticated}
+          exact
+        />
+        {/* Private Routes */}
+        <Route
+          element={<PrivateOutlet userIsAuthenticated={userIsAuthenticated} />}
+        >
+          <Route element={<HomeLoggedIn />} path="/HomeLoggedIn" exact />
+        </Route>
+
+        {/* Other Routes */}
+        <Route
+          path="/unauthorised"
+          element={<p>User is unauthorised</p>}
+          exact
+        />
+        <Route path="/error" element={<p>Page error</p>} exact />
+        <Route path="*" element={<p>Path not resolved</p>} />
+
+      </RRDRoutes>
+    </Router>
+  );
+};
+
+export default Routes;
